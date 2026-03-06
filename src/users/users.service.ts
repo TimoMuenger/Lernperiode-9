14a7368be4848146d33b5d5e7c2f3a791c83e284
Timo Münger <t.muenger.inf24@stud.bbbaden.ch>
2026-03-06 11:42:38 +0100

    Add files via upload

---
+++ src/users/users.service.ts
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
+@Injectable()
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      role: 'ADMIN',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      role: 'ENGINEER',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      const rolesArray = this.users.filter((user) => user.role === role);
+
+      if (rolesArray.length === 0)
+        throw new NotFoundException('User role not found');
+      return rolesArray;
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) throw new NotFoundException('User not Found');
+    return user;
+  }
+
+  create(user: CreateUserDto) {
+    const userByHighestID = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = { id: userByHighestID[0].id + 1, ...user };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updatedUser: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
+}
